18033411e6bf984f11fb81e0846d3be047856567
Its-Ani-Vibe <ani.vibe.done@gmail.com>
2026-09-05 03:00:57 +0600

    Problem 1 Completed

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
 // Problem 1 
+function studentIntroduction(student) {
+    student ={
+      name: "Rafi",
+      age: 18,
+    course: "JavaScript"
+
+   }
+  if(typeof student !== "object" || Object.keys(student).length===0 ){
+    return "Invalid"
+  }
+  else{
+  return `My name is ${student?.name}. I am ${student?.age} years old. I am learning ${student?.course}.`;
+    }
+}
+
+console.log(studentIntroduction());
+
+//Problem 1 complete 
